7424608a7e1264510467a3bdf9d11daeec242fd0
minam2593 <minam2593@naver.com>
2024-10-04 14:45:59 +0900

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,19 +1,1 @@
-// Smooth scroll for navigation links
-document.querySelectorAll('nav a').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
-        const targetId = this.getAttribute('href').substring(1);
-        const targetSection = document.getElementById(targetId);
-        targetSection.scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
 
-// 햄버거 메뉴 클릭 시 작동하는 스크립트
-const hamburger = document.querySelector('.hamburger');
-const navLinks = document.querySelector('.nav-links');
-
-hamburger.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-});
